bb35c48f631ee2da653bacda129d75fe744c86ac
robrao <rao.robby@gmail.com>
2017-04-06 22:39:58 +0000

    return json from process_image function

--- eval_image.py
+++ eval_image.py
@@ -16,10 +16,12 @@ slim = tf.contrib.slim
 gpu_options = tf.GPUOptions(allow_growth=True)
 config = tf.ConfigProto(log_device_placement=False, gpu_options=gpu_options)
 
+classifier_id = "SSD_512_VOC07+12+COCO"
 net_shape = (512, 512)
 data_format = 'NHWC'
 img_path = 'demo/'
 classes = np.asarray(['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor'])
+osprey_classes = np.asarray(['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'vehicle', 'vehicle', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'pedestrian', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor'])
 
 img_input = tf.placeholder(tf.uint8, shape=(None, None, 3))
 
@@ -35,7 +37,7 @@ with slim.arg_scope(ssd_net.arg_scope(data_format=data_format)):
     predictions, localisations, _, _ = ssd_net.net(image_4d, is_training=False, reuse=reuse)
     
 # Main image processing routine.
-def process_image(img, sess, select_threshold=0.5, nms_threshold=.45, net_shape=(512, 512)):
+def process_image(img, sess, classes=osprey_classes, ssd_id=classifier_id, select_threshold=0.5, nms_threshold=.45, net_shape=(512, 512)):
     # Run SSD network.
     rimg, rpredictions, rlocalisations, rbbox_img = sess.run([image_4d, predictions, localisations, bbox_img], feed_dict={img_input: img})
                 
@@ -48,7 +50,16 @@ def process_image(img, sess, select_threshold=0.5, nms_threshold=.45, net_shape=
 
     # Resize bboxes to original image shape. Note: useless for Resize.WARP!
     rbboxes = np_methods.bboxes_resize(rbbox_img, rbboxes)
-    return rclasses, rscores, rbboxes
+
+    results = []
+    for idx, rc in enumerate(rclasses):
+        c = classes[rc-1]
+        pc = rscores[idx]
+        bbx = rbboxes[idx]
+        
+        results.append({'classifier_id':ssd_id, 'class_name':c, 'class_probability':pc, 'bbx':list(bbx)})
+
+    return results
 
 def main(_):
     ckpt_path = '/data/ssd_models/vgg_512/VGG_VOC0712_SSD_512x512_ft_iter_120000.ckpt'
@@ -60,13 +71,9 @@ def main(_):
         img_names = sorted(os.listdir(img_path))
         #img = mpimg.imread(path + image_names[-5])
         img = Image.open(img_path + img_names[-5])
-        rclasses, rscores, rbboxes = process_image(img, sess)
-    
-        found_classes = []
-        for c in rclasses:
-           found_classes.append(classes[c-1])
+        results = process_image(img, sess)
         
-        print("class_indexes:{}\nclasses: {}\nscores:{}\nbbxs: {}\n".format(rclasses, found_classes, rscores, rbboxes))
+        print(results)
 
 if __name__ == '__main__':
     tf.app.run()
